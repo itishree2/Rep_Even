cddddeda0d6f04b9d981eab5533ca55093a3ca8e
itishree2 <nayakitishree6262@gmail.com>
2024-02-16 09:43:16 +0530

    first commited

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+Console.WriteLine("Welcome to number system");
+Console.WriteLine("***Odd number***");
+NumberSystem.OddNum();
+Console.WriteLine("***Even number***");
+NumberSystem.EvenNum();
